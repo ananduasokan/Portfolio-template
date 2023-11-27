a36da55fb8ba6b67f4015e39e73169b3a21cd1d5
ananduasokan <100429684+ananduasokan@users.noreply.github.com>
2023-11-27 09:02:25 +0530

    Add files via upload

---
+++ myForm/script.js
@@ -0,0 +1,82 @@
+let nameError = document.getElementById('name-error');
+let phoneError = document.getElementById('phone-error');
+let emailError = document.getElementById('email-error');
+let messageError = document.getElementById('message-error');
+let submitError = document.getElementById('submit-error');
+
+const validateName = () => {
+    let name = document.getElementById('contact-name').value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false
+    }
+
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write full name';
+        return false
+    }
+    nameError.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+    return true
+}
+
+const validatePhone = () => {
+    let phone = document.getElementById('contact-phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone is required';
+        return false;
+    }
+
+    if (phone.length !== 10) {
+        phoneError.innerHTML = 'Phone no should be 10 digits';
+        return false;
+    }
+
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = 'Only digits';
+        return false;
+    }
+
+    phoneError.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+    return true
+}
+
+const validateEmail = () => {
+    let email = document.getElementById('contact-email').value;
+
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+
+    if (!email.match(/^[A-Za-z\._\-[0-9]*@[A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email invalid';
+        return false;
+    }
+
+    emailError.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+    return true;
+}
+
+const validateMessage = () => {
+    let message = document.getElementById('contact-message').value;
+    let required = 30;
+    let left = required - message.length;
+
+    if (left > 0) {
+        messageError.innerHTML = left + ' more characters required';
+        return false;
+    }
+
+    messageError.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+    return true;
+
+}
+
+const validateForm = () => {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        messageError.innerHTML = 'enter all details';
+        return false
+    }
+}
